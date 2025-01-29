6cee4b118cc0b1a6acae9b974ffee28ce4a3df08
FRANKLIN DANIEL ZAMORA GUTIERREZ <fzamora@paquetexpress.com.mx>
2025-01-28 22:16:11 -0700

    feat

---
+++ main.js
@@ -0,0 +1,40 @@
+window.addEventListener('scroll', function() {
+  const header = document.getElementById('header');
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+// Inicializar EmailJS
+document.addEventListener('DOMContentLoaded', function() {
+  emailjs.init("MKDm68Kk9JuSeRDtu");
+
+  // Manejador de eventos para el formulario
+  const contactForm = document.getElementById('contact-form');
+  if (contactForm) {
+      contactForm.addEventListener('submit', function(event) {
+          event.preventDefault();
+          emailjs.sendForm('service_in5rwpi', 'template_7ibvtdu', this)
+              .then(function() {
+                  console.log('SUCCESS!');
+                  const modal = document.getElementById('emailSentModal');
+                  const span = document.getElementsByClassName('close')[0];
+                  modal.style.display = 'block';
+                  span.onclick = function() {
+                      modal.style.display = 'none';
+                      location.reload(); // Refrescar la página al cerrar el modal
+                  }
+                  window.onclick = function(event) {
+                      if (event.target == modal) {
+                          modal.style.display = 'none';
+                          location.reload(); // Refrescar la página al cerrar el modal
+                      }
+                  }
+              }, function(error) {
+                  console.log('FAILED...', error);
+              });
+      });
+  }
+});
